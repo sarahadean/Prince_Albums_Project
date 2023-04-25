ddc945592f6a43250696ce16448a7c24bc23e93f
Eric <macario41@gmail.com>
2023-04-25 18:03:54 -0400

    stuff

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// document.addEventListener('DOMContentLoaded', rover())
-// const url = "http://localhost:3000/albums"
-// const albumNav = document.getElementById('album_nav')
-
-// function rover(urlParam){
-// return fetch(urlParam)
-// .then(resp => resp.json())
-// }
-
-// rover(url).then(data => data.map((album => {
-//     const albumImg = document.createElement('img')
-
-//     albumImg.src = album.image
-//     albumNav.appendChild(albumImg)
-// }
-// )))
-
-
-
-// console.log(albumNav)
-
 document.addEventListener('DOMContentLoaded', function() {
     fetch ('http://localhost:3000/albums')
     .then (response => response.json())
