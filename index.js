f7b5304429e99d9c1d0ffef43aa1eb087d1b1b7e
Eric <macario41@gmail.com>
2023-04-25 17:52:08 -0400

    click events

--- index.js
+++ index.js
@@ -1,20 +1,73 @@
-document.addEventListener('DOMContentLoaded', rover())
-const url = "http://localhost:3000/albums"
-const albumNav = document.getElementById('album_nav')
+// document.addEventListener('DOMContentLoaded', rover())
+// const url = "http://localhost:3000/albums"
+// const albumNav = document.getElementById('album_nav')
 
-function rover(urlParam){
-return fetch(urlParam)
-.then(resp => resp.json())
+// function rover(urlParam){
+// return fetch(urlParam)
+// .then(resp => resp.json())
+// }
+
+// rover(url).then(data => data.map((album => {
+//     const albumImg = document.createElement('img')
+
+//     albumImg.src = album.image
+//     albumNav.appendChild(albumImg)
+// }
+// )))
+
+
+
+// console.log(albumNav)
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetch ('http://localhost:3000/albums')
+    .then (response => response.json())
+    .then (album => {
+        const albumNav = document.getElementById("album_nav")
+        const firstAlbum = album[0]
+
+        album.forEach(album => {
+            const albumImage = document.createElement("img")
+            albumImage.src = album.image
+            albumImage.alt = album.name      
+            albumImage.addEventListener('click', function() {
+                displayAlbum(album)
+            })
+            albumNav.appendChild(albumImage)
+        });
+        displayAlbum(firstAlbum)
+    })
+})
+
+function displayAlbum(album) {
+    const displayImage = document.getElementById('album-image')
+    displayImage.src = album.image
+    const displayTitle = document.getElementById('title')
+    displayTitle.textContent = album.name
+    const displayYear = document.getElementById('year')
+    displayYear.textContent = album.year_released
+    const displayList = document.getElementById('tracklist')
+
+    displayList.textContent = ' '
+
+    album.tracks.forEach(track => {
+        const trackSong = document.createElement('li')
+        const trackButton = document.createElement('button')
+        trackSong.textContent = ` ${track} `
+        trackButton.textContent = "+"
+        trackButton.addEventListener('click', () => {trackFavorites()})
+        displayList.appendChild(trackSong)
+        trackSong.appendChild(trackButton)
+
+    })
+
 }
 
-rover(url).then(data => data.map((album => {
-    const albumImg = document.createElement('img')
+function trackFavorites(track) {
+    const hrTrack = getElementById('faveTrack')
+    const favoriteTrack = document.createElement('li')
+    favoriteTrack.textContent = e.target.parentNode
+    hrTrack.appendChild(favoriteTrack)
+    e.preventDefault()
 
-    albumImg.src = album.image
-    albumNav.appendChild(albumImg)
 }
-)))
-
-
-
-console.log(albumNav)
