28fd3a3f7315f797e6e75d4d39a05a2789882bad
Eric <macario41@gmail.com>
2023-04-26 14:03:30 -0400

    post request

--- index.js
+++ index.js
@@ -37,23 +37,54 @@ function displayAlbum(album) {
         trackButton.addEventListener('click', trackFavorites)
         displayList.appendChild(trackSong)
         trackSong.appendChild(trackButton)
-
+        
     })
-
 }
 
 function trackFavorites(e) {
     e.preventDefault()
     const trackName = e.target.parentNode.textContent
-    const trackFavorites = document.getElementById('faveTrack')
-    const newTrack = document.createElement('li')
-    const removeButton = document.createElement('buutton')
-    removeButton.textContent = 'remove'
-    removeButton.addEventListener('click', () => {
-        trackFavorites.removeChild(newTrack)
-    })
-    newTrack.textContent = trackName
-    newTrack.appendChild(removeButton)
-    trackFavorites.appendChild(newTrack)
 
-}
+    fetch ('http://localhost:3000/favorites', {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: trackName,
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            const trackFavorites = document.getElementById('faveTrack')
+            const newTrack = document.createElement('li')
+            const removeButton = document.createElement('button')
+            removeButton.textContent = 'remove'
+            removeButton.setAttribute = ('id', 'delete-btn') 
+            removeButton.addEventListener('click', (e) => {
+                byeByeBye(e)
+                e.preventDefault()
+            })
+            newTrack.textContent = trackName
+            newTrack.appendChild(removeButton)
+            trackFavorites.appendChild(newTrack)
+        })
+    }
+
+    function byeByeBye(e, id) {
+        const trackFavorite = document.getElementById('faveTrack')
+        const byeParent = e.target.parentNode
+        // const id = byeParent.target.id
+
+        fetch (`http://localhost:3000/favorites/${id}` + id , {
+            method: 'DELETE',
+            headers: {
+                "content-type": "application/json",
+                accept: "application/json"
+                }
+        })
+        .then(response => response.json())
+        .then(data => {
+            trackFavorite.removeChild(byeParent)
+        })
+    }
