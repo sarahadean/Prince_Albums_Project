d2aef7e0bc2b884fdb4ea87ce68d609a3380296e
Sarah Dean <sarah.schmidtdean@gmail.com>
2023-04-25 21:02:05 -0500

    js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-document.addEventListener('DOMContentLoaded', rover())
+
+document.addEventListener('DOMContentLoaded', function(){
 const url = "http://localhost:3000/albums"
 const albumNav = document.getElementById('album_nav')
 
-function rover(urlParam){
-return fetch(urlParam)
+fetch(url)
 .then(resp => resp.json())
-}
-
-rover(url).then(data => data.map((album => {
+.then(data => {
+    data.map(album => {
     const albumImg = document.createElement('img')
-
     albumImg.src = album.image
     albumNav.appendChild(albumImg)
-}
-)))
+})
+})
 
-
-
-console.log(albumNav)
+})
